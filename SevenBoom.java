c9d84f73cab4f375fa8a4ba24da1ec6b365d65f5
HillaShx <hilla.sh@gmail.com>
2019-04-21 12:56:00 +0300

    submission of exercise 2.4

---
+++ SevenBoom.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class SevenBoom {
+    public static void main(String args[]) {
+        /* This program prints out numbers 1-100. Whenever it gets to a number that either comntains the digit 7 or is divisible by 7, it prints out "BOOM". */
+        Double i;
+        for (i=1.0;i<=100;i++) {
+            if (((i / 7) % 1) == 0) {
+                System.out.print("BOOM,");
+            } else if ((i % 10) == 7) {
+                System.out.print("BOOM,");
+            } else if (((i / 10) >= 7) && ((i / 10) < 8)) {
+                System.out.print("BOOM,");
+            } else if (i == 100) {
+                System.out.println(i.intValue());
+            } else {
+                System.out.print(i.intValue()+",");
+            }
+        }
+    }
+}
