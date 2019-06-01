eca28c4e45a8a3743bdb76b2032c7f7d4594bf32
HillaShx <hilla.sh@gmail.com>
2019-06-01 15:59:24 +0300

    final assignment3 010619

--- 00 Assignment3_Quiz/js/quiz.js
+++ 00 Assignment3_Quiz/js/quiz.js
@@ -1,28 +1,30 @@
-var questions = {
-  question1: {
-    question: "What is your favorite flavor of ice-cream?",
-    answers: [
-      {answer_0: "I had ice-cream with the queen once"},
-      {answer_1: "The one with whipped cream on top and a shrimp cocktail on the side"},
-      {answer_2: "Uuuuhhhhh..."},
-      {answer_3: "Tacos"}
-    ]
-  },
-  question2: {
-    question: "What's your idea of fun?",
-    answers: [
-      {answer_0: "Shrimp cocktails and tequila"},
-      {answer_1: "Define \"fun\""},
-      {answer_2: "My idea is to have a good time and then it'll be fun"},
-      {answer_3: "I enjoy people being jealous of me, especially my sister"}
-    ]
-  },
+var GoodPlaceQuiz = {
+  questions:
+  {
+    question1: {
+      question: "What is your favorite flavor of ice-cream?",
+      answers: [
+        {answer_0: "I had ice-cream with the queen once"},
+        {answer_1: "The one with whipped cream on top and a shrimp cocktail on the side"},
+        {answer_2: "Uuuuhhhhh..."},
+        {answer_3: "Tacos"}
+        ]
+    },
+    question2: {
+      question: "What's your idea of fun?",
+      answers: [
+        {answer_0: "Shrimp cocktails and tequila"},
+        {answer_1: "Discussing moral philosophy"},
+        {answer_2: "My idea is to have a good time and then it'll be fun"},
+        {answer_3: "I enjoy people being jealous of me, especially my sister"}
+      ]
+      },
   question3: {
     question: "What do you do for a living?",
     answers: [
       {answer_0: "PAR-TEEEEEEY"},
       {answer_1: "Ugh!! doing things \"for a living\", how BORING! let's talk about something else!"},
-      {answer_2: "What do you mean, darling?"},
+      {answer_2: "What do you mean?"},
       {answer_3: "I teach a subject I'm highly passionate about"}
     ]
   },
@@ -32,7 +34,7 @@ var questions = {
       {answer_0: "MOTHERFORKER"},
       {answer_1: "Uuuuuhhhh..."},
       {answer_2: "Hey, I'm in line? Nice! What for?"},
-      {answer_3: "Oh, I don't stand in line, dear"}
+      {answer_3: "Standing in line isn't really my style"}
     ]
   },
   question5: {
@@ -43,49 +45,46 @@ var questions = {
       {answer_2: "My best friend because he can always make me laugh"},
       {answer_3: "My wallet and keys. No, my car and my books. No! my..."}
     ]
-  },
-  // question6: {
-  //   question: "",
-  //   answer_a: "",
-  //   answer_b: "",
-  //   answer_c: "",
-  //   answer_d: ""
-  // },
-  // question7: {
-  //   question: "",
-  //   answer_a: "",
-  //   answer_b: "",
-  //   answer_c: "",
-  //   answer_d: ""
-  // },
-  // question8: {
-  //   question: "",
-  //   answer_a: "",
-  //   answer_b: "",
-  //   answer_c: "",
-  //   answer_d: ""
-  // },
-  // question9: {
-  //   question: "",
-  //   answer_a: "",
-  //   answer_b: "",
-  //   answer_c: "",
-  //   answer_d: ""
-  // },
-  // question10: {
-  //   question: "",
-  //   answer_a: "",
-  //   answer_b: "",
-  //   answer_c: "",
-  //   answer_d: ""
-  // }
+  }
+},
+  results: [
+    {
+      name: "Tahani Al-Jameel",
+      img: "./img/avatars/tahani.jpg",
+      description: "You are Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      points: 0
+    },
+    {
+      name: "Chidi Anagonye",
+      img: "./img/avatars/chidi.jpg",
+      description: "You are Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      points: 0
+    },
+    {
+      name: "Jason Mandoza",
+      img: "./img/avatars/jason.jpg",
+      description: "You are Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      points: 0
+    },
+    {
+      name: "Eleanor Shellstrop",
+      img: "./img/avatars/eleanor.jpg",
+      description: "You are Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      points: 0
+    }
+  ]
 }
 
 
-var eleanor = 0;
-var jason = 0;
-var tahani = 0;
-var chidi = 0;
+var charactersArray = GoodPlaceQuiz.results;
+// charactersArray: 0-tahani, 1-chidi, 2-jason, 3-eleanor
+
+// q1: 0-t,1-e,2-c,3-j;
+// q2: 0-e,1-c,2-j,3-t;
+// q3: 0-j,1-e,2-t,3-c;
+// q4: 0-e,1-c,2-j,3-t;
+// q5: 0-t,1-e,2-j,3-c;
+
 var questionNumber = 0;
 var slideshowDOM = document.getElementById("slideshow");
 
@@ -98,71 +97,81 @@ function getAnswersObjectFromDOM() {
 }
 
 function insertQuestionItemsToSlide(qslide) {
-  // insert each item object to slide. iterate the function call until you are out of questions.
   var questionDOM = getQuestionObjectFromDOM();
   var answersDOM = getAnswersObjectFromDOM();
-  questionDOM.innerHTML = questions["question" + qslide].question;
-  var answerNumInObj = 0
+  questionDOM.innerHTML = GoodPlaceQuiz.questions["question" + qslide].question;
+  var answerNumInObj = 0;
   for (var i=0; i<answersDOM.childElementCount; i++) {
     for (var j=0; j<answersDOM.children[i].childElementCount; j++) {
-      answersDOM.children[i].children[j].innerHTML = questions["question" + qslide].answers[answerNumInObj]["answer_" + answerNumInObj];
+      answersDOM.children[i].children[j].innerHTML = GoodPlaceQuiz.questions["question" + qslide].answers[answerNumInObj]["answer_" + answerNumInObj];
       answerNumInObj++;
     }
   }
 }
 
-// insertQuestionItemsToSlide(questionNumber);
+function prevSlide() {
+  if (questionNumber === 1) {
+    var activeSlide = document.getElementsByClassName("active-slide")[0];
+    activeSlide.classList.remove("active-slide");
+    var openingSlide = document.getElementById("opening-slide");
+    openingSlide.className += " active-slide";
+    activeSlide.classList.remove("active-slide");
+    questionNumber--;
+    hideNavigation();
+  } else {
+    questionNumber--;
+    insertQuestionItemsToSlide(questionNumber);
+  }
+}
 
-// q1: 0-t,1-e,2-c,3-j;
-// q2: 0-e,1-c,2-j,3-t;
-// q3: 0-j,1-e,2-t,3-c;
-// q4: 0-e,1-c,2-j,3-t;
-// q5: 0-t,1-e,2-j,3-c;
 
 function nextSlide() {
   if (!document.getElementsByClassName("active-answer")[0]) {
-    document.getElementById("") // THIS IS WHERE I STOPPED. Make error div visible;
+    popAlert();
+  } else if (questionNumber === Object.keys(GoodPlaceQuiz.questions).length) {
+    turnToResult();
+    questionNumber = 0;
   } else {
     var chosenAnswer = parseInt(document.getElementsByClassName("active-answer")[0].getAttribute('value'));
     switch (questionNumber) {
       case 1:
         switch (chosenAnswer) {
-          case 0: tahani++; break;
-          case 1: eleanor++; break;
-          case 2: chidi++; break;
-          case 3: jason++; break;
+          case 0: charactersArray[0].points++; break;
+          case 1: charactersArray[3].points++; break;
+          case 2: charactersArray[1].points++; break;
+          case 3: charactersArray[2].points++; break;
         }
         break;
       case 2:
         switch(chosenAnswer) {
-          case 0: eleanor++; break;
-          case 1: chidi++; break;
-          case 2: jason++; break;
-          case 3: tahani++; break;
+          case 0: charactersArray[3].points++; break;
+          case 1: charactersArray[1].points++; break;
+          case 2: charactersArray[2].points++; break;
+          case 3: charactersArray[0].points++; break;
         }
         break;
       case 3:
         switch (chosenAnswer) {
-          case 0: jason++; break;
-          case 1: eleanor++; break;
-          case 2: tahani++; break;
-          case 3: chidi++; break;
+          case 0: charactersArray[2].points++; break;
+          case 1: charactersArray[3].points++; break;
+          case 2: charactersArray[0].points++; break;
+          case 3: charactersArray[1].points++; break;
         }
         break;
       case 4:
         switch (chosenAnswer) {
-          case 0: eleanor++; break;
-          case 1: chidi++; break;
-          case 2: jason++; break;
-          case 3: tahani++; break;
+          case 0: charactersArray[3].points++; break;
+          case 1: charactersArray[1].points++; break;
+          case 2: charactersArray[2].points++; break;
+          case 3: charactersArray[0].points++; break;
         }
         break;
       case 5:
         switch (chosenAnswer) {
-          case 0: tahani++; break;
-          case 1: eleanor++; break;
-          case 2: jason++; break;
-          case 3: chidi++; break;
+          case 0: charactersArray[0].points++; break;
+          case 1: charactersArray[3].points++; break;
+          case 2: charactersArray[2].points++; break;
+          case 3: charactersArray[1].points++; break;
       }
     }
     questionNumber++;
@@ -186,9 +195,34 @@ function turnToQuestions() {
   revealNavigation();
 }
 
+function turnToResult() {
+  // result slide
+  hideNavigation();
+  var activeSlide = document.getElementsByClassName("active-slide")[0];
+  activeSlide.classList.remove("active-slide");
+  var resultSlide = document.getElementById("result-slide");
+  resultSlide.className += " active-slide";
+  var resultChar = {};
+  var resultPoints = 0;
+  for (var i = 0; i < GoodPlaceQuiz.results.length; i++) {
+    if (charactersArray[i].points > resultPoints) {
+      resultChar = charactersArray[i];
+      resultPoints = charactersArray[i].points;
+    }
+  }
+  document.getElementById("result-ava").src = resultChar.img;
+  document.getElementById("result-anouncement").innerText += " " + resultChar.name;
+  document.getElementById("result-description").innerText = resultChar.description
+}
+
 function revealNavigation() {
   document.getElementsByClassName("nav-btn")[0].classList.remove("hide");
   document.getElementsByClassName("nav-btn")[1].classList.remove("hide");
+}
+
+function hideNavigation() {
+  document.getElementsByClassName("nav-btn")[0].className += " hide";
+  document.getElementsByClassName("nav-btn")[1].className += " hide";
 }
 
 var answersDOM = slideshowDOM.getElementsByClassName("answer");
@@ -202,5 +236,28 @@ for (var i=0; i<answersDOM.length; i++) {
     }
   
     this.className += " active-answer";
+    document.getElementsByClassName("nav-btn")[1].style.fontWeight = "bold";
   });
+}
+
+var charactersDOM = document.getElementsByClassName("char-avatar");
+var charNum = 0;
+for (var i=0; i<charactersDOM.length; i++) {
+  charNum=i;
+  charactersDOM[i].addEventListener("mouseover", function() {
+    // var current = document.getElementById("which-are-you");
+  
+    document.getElementById("which-are-you").innerText = this.alt + "?";
+    // this.innerText = charactersArray[charNum].name;
+  });
+}
+
+function popAlert() {
+  document.getElementById("no-chosen-answer-alert-screen").style.visibility = "visible";
+  document.getElementById("no-chosen-answer-alert-screen").style.zIndex = "2";
+}
+
+function hideAlert() {
+  document.getElementById("no-chosen-answer-alert-screen").style.visibility = "hidden";
+  document.getElementById("no-chosen-answer-alert-screen").style.zIndex = "-1";
 }
