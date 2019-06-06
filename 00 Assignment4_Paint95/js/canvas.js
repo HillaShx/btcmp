7fb311f0958985600009918300eae3377a7c0bcb
HillaShx <hilla.sh@gmail.com>
2019-06-06 17:50:09 +0300

    brush can paint, can't pick a color, can't change crush properties

--- 00 Assignment4_Paint95/js/canvas.js
+++ 00 Assignment4_Paint95/js/canvas.js
@@ -15,7 +15,10 @@ var paint95 = {
     "#56eef4",
     "#32e875",
     "#98d2eb"
-  ]
+  ],
+  brush: {
+    width: 1,
+  }
 }
 
 function initPaint() { 
@@ -30,7 +33,7 @@ function initPaint() {
   canvas.css("background-color", paint95.canvas.backgroundColor);
   paintingTitle.appendTo(paintHeader);
   paintHeader.appendTo("body");
-  canvas.appendTo($("body"));
+  canvas.appendTo("body");
   colorOnBrush = initPalette();
 }
 
@@ -44,9 +47,51 @@ function initPalette() {
     currColor.appendTo(colorPalette);
   }
   colorPalette.appendTo("body")
-  var colorOnBrush = paint95.colorPalette[0];
-  paint95.meta.colorOnBrush = colorOnBrush;
+  var currColor = $("<div/>");
+  currColor.attr("id", "brush");
+  var colorOnBrush = paint95.colorPalette[0]; 
+  currColor.css("background-color", colorOnBrush);
+  var colorIndicator = $("<span/>");
+  colorIndicator.attr("id", "color-indicator");
+  paint95.brush.colorOnBrush = colorOnBrush;
+  colorIndicator.text("brush color:")
+  colorIndicator.appendTo(colorPalette);
+  currColor.appendTo(colorPalette);
 }
 
 initPaint();
+
+var isClicked = false;
+
+
+function drawing(dotX,dotY) {
+  if (isClicked) {
+    var dot = $("<div/>");
+    dot.attr("class", "dot");
+    dot.css("background-color", paint95.brush.colorOnBrush);
+    dot.css("top", dotY + "px");
+    dot.css("left", dotX + "px");
+    dot.appendTo($("#canvas"));
+  }  
+}
+
+
+$("#canvas").on("mousedown", function(e) {
+  isClicked = true;
+  var dotPosY = e.pageY - $("#canvas").offset().top;
+  var dotPosX = e.pageX - $("#canvas").offset().left;
+  drawing(dotPosX,dotPosY);
+ 
+});
+
+
+$("#canvas").on("mousemove", function(e) {
+  var dotPosY = e.pageY - $("#canvas").offset().top;
+  var dotPosX = e.pageX - $("#canvas").offset().left;
+  drawing(dotPosX,dotPosY);
+});
+
+$(document).on("mouseup", function() {
+  isClicked = false;
+});
 
