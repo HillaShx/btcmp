09940c4c7960415c81b9540dc0a5855feff0354c
HillaShx <hilla.sh@gmail.com>
2019-06-06 15:09:57 +0300

    defined initializer for canvas and palette

--- 00 Assignment4_Paint95/js/canvas.js
+++ 00 Assignment4_Paint95/js/canvas.js
@@ -18,17 +18,35 @@ var paint95 = {
   ]
 }
 
-var paintHeader = $("<div/>");
-paintHeader.addClass("header");
-var PaintTitle
-var paintingTitle = $("<h1/>");
-paintingTitle.text(`${paint95.canvas.name} - ${paint95.meta.title}`);
-var canvas = $("<div/>");
-canvas.attr("id","canvas");
-canvas.css("width", paint95.canvas.width);
-canvas.css("height", paint95.canvas.height);
-canvas.css("background-color", paint95.canvas.backgroundColor);
-canvas.css("position", "absolute")
-paintingTitle.appendTo(paintHeader);
-paintHeader.appendTo("body");
-canvas.appendTo($("body"));
+function initPaint() { 
+  var paintHeader = $("<div/>");
+  paintHeader.addClass("header");
+  var paintingTitle = $("<h1/>");
+  paintingTitle.text(`${paint95.canvas.name} - ${paint95.meta.title}`);
+  var canvas = $("<div/>");
+  canvas.attr("id","canvas");
+  canvas.css("width", paint95.canvas.width);
+  canvas.css("height", paint95.canvas.height);
+  canvas.css("background-color", paint95.canvas.backgroundColor);
+  paintingTitle.appendTo(paintHeader);
+  paintHeader.appendTo("body");
+  canvas.appendTo($("body"));
+  colorOnBrush = initPalette();
+}
+
+function initPalette() {
+  var colorPalette = $("<div/>");
+  colorPalette.attr("id", "color-palette");
+  for (var i=0; i<paint95.colorPalette.length; i++) {
+    var currColor = $("<div/>");
+    currColor.addClass("color-in-palette");
+    currColor.css("background-color", paint95.colorPalette[i]);
+    currColor.appendTo(colorPalette);
+  }
+  colorPalette.appendTo("body")
+  var colorOnBrush = paint95.colorPalette[0];
+  paint95.meta.colorOnBrush = colorOnBrush;
+}
+
+initPaint();
+
