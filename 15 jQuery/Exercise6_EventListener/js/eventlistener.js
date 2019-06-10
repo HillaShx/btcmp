3d1fa1cd2e83e07de9ae2915860e6ec65f0dfa7f
HillaShx <hilla.sh@gmail.com>
2019-06-10 10:29:32 +0300

    fixed star 6 in advanced js jquery

--- 15 jQuery/Exercise6_EventListener/js/eventlistener.js
+++ 15 jQuery/Exercise6_EventListener/js/eventlistener.js
@@ -1,14 +1,12 @@
 var textfields = $("input");
-var textItems = [];
 function getTextValues() {
+  var textItems = [];
   textfields.each(function(item) {
-    textItems.push($(item).val());
+    textItems.push($(this).val());
   });
+  return textItems.join(" ");
 };
 
 textfields.on("keyup",function(e) {
-  // $(".results").text(`${$(textfields[0]).val()} ${$(textfields[1]).val()} ${$(textfields[2]).val()} ${$(textfields[3]).val()}`)
-  // getTextValues();
-  // var resultStr = textItems.join(" ");
-  $(".results")[0].text(getTextValues());
+  $("#results").text(getTextValues());
 });
