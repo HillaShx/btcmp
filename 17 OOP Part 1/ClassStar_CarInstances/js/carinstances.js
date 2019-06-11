718ef3ae7a965c800e1665fa2d03f1c33d6f6a2d
HillaShx <hilla.sh@gmail.com>
2019-06-11 10:34:16 +0300

    class strs

---
+++ 17 OOP Part 1/ClassStar_CarInstances/js/carinstances.js
@@ -0,0 +1,10 @@
+class Car {
+  constructor(color, age) {
+    this.color = color;
+    this.age = age;
+  }
+}
+
+let binba = new Car("yellow", 3);
+let binbi = new Car("orange", 5);
+let binbu = new Car("red", 7);
