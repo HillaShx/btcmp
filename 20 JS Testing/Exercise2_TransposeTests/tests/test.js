0e53e15f315c68b6c25dd83a990108f37b38073b
HillaShx <hilla.sh@gmail.com>
2019-06-18 15:15:51 +0300

    js testing exercise2

---
+++ 20 JS Testing/Exercise2_TransposeTests/tests/test.js
@@ -0,0 +1,17 @@
+function transposeArray(arr) {
+  var transArray = [];
+  for (var i=0; i<arr.length; i++) {
+    var rowLen = arr[i].length;
+    transArray.push([]);
+    for (var j=0; j<rowLen; j++) {
+      transArray[i].push(arr[j][i]);
+    }
+  }
+  return transArray;
+}
+
+let expect = require('chai').expect;
+
+it('should return a transposed array', ()=> {
+  expect(transposeArray([[2,3],[4,5]])).is.eql([[2,4],[3,5]]);
+});
