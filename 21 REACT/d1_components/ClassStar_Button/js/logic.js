cde1cc78c08e36cf817faa489cede96f2bcc865c
HillaShx <hilla.sh@gmail.com>
2019-06-23 15:12:23 +0300

    react components classstar_button

---
+++ 21 REACT/d1_components/ClassStar_Button/js/logic.js
@@ -0,0 +1,12 @@
+class Button extends React.Component {
+    render() {
+        return (
+            <button>Press Me</button>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Button/>,
+    document.getElementById("root")
+);
