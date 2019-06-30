a5b00e98ced982f2961264fffb24eb5297f66747
HillaShx <hilla.sh@gmail.com>
2019-06-30 16:12:28 +0300

    react state exercise4_notifications

---
+++ 21 REACT/d4_state/Exercise4_Notifications/js/logic.js
@@ -0,0 +1,47 @@
+class MenuBar extends React.Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div className="menu-bar">
+                <Icon hasNotif={false} imgUrl='./img/users-icon.png' />
+                <Icon hasNotif={false} imgUrl='./img/search-icon.png' />
+                <Icon hasNotif={true} imgUrl='./img/messages-icon.png' />
+            </div>
+        )
+    }
+}
+
+class Icon extends React.Component {
+    constructor() {
+        super();
+        this.popBubble = this.popBubble.bind(this);
+        this.state = {
+            numOfNotif: Math.floor(Math.random() * 11),
+            isClicked: false
+        }
+    }
+
+    popBubble() {
+        this.setState({
+            isClicked: true
+        })
+    }
+
+    render() {
+        let toShow = this.props.hasNotif && !this.state.isClicked && (this.state.numOfNotif > 0);
+        console.log(toShow);
+        return (
+            <div onClick={this.popBubble} className="icon" style={{backgroundImage: `url(${this.props.imgUrl})`}}>
+                <div className={`notifications` + (toShow ? "" : " hide")}>{this.state.numOfNotif}</div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <MenuBar />,
+    document.getElementById("root")
+);
