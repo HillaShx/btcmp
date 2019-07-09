eb937c51569de106784ef964b9f7a4039b6914f7
HillaShx <hilla.sh@gmail.com>
2019-07-09 14:01:44 +0300

    correction to exercise3_numbersdigits

--- 23 Python/Intro to Python/Exercise3_NumbersDigits/numbersdigits.py
+++ 23 Python/Intro to Python/Exercise3_NumbersDigits/numbersdigits.py
@@ -1,11 +1,13 @@
-def analyze_numbers_digits(num):
+def analyze_numbers_digits():
+  n = input('enter a number:\n')
   sum_of_digits = 0
-  print("you've entered the number " + str(num))
-  print("the digits of this number are:")
-  for digit in str(num):
-    print(int(digit), end=",")
+  print(f"you've entered the number {n}")
+
+  print("the digits of this number are:", end=" ")
+  for digit in n:
+    print(int(digit), end=", ")
     sum_of_digits+=int(digit)
   print()
-  print("the sum of these digits is " + str(sum_of_digits))
+  print(f"the sum of these digits is {sum_of_digits}")
 
-analyze_numbers_digits(88888)
+analyze_numbers_digits()
