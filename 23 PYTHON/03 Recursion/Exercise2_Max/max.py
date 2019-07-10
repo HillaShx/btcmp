cf3ef6ecad777f341b1e5858ca16b33a3639b56e
HillaShx <hilla.sh@gmail.com>
2019-07-10 14:19:07 +0300

    exercise2_max

---
+++ 23 PYTHON/03 Recursion/Exercise2_Max/max.py
@@ -0,0 +1,10 @@
+def max_in_list(l, i): 
+    if (i == len(l)-1): 
+        return l[-1]
+    if l[i+1] > max_in_list(l,i+1):
+      return l[i+1]
+    else:
+      return max_in_list(l,i+1)
+
+print(max_in_list([3,4,15,6,9,22],0))
+  
