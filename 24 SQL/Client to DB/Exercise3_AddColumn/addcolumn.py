c1fd1644cecf583de73004c0c748ba634f658764
HillaShx <hilla.sh@gmail.com>
2019-07-23 18:12:14 +0300

    exercise3_addcolumn

---
+++ 24 SQL/Client to DB/Exercise3_AddColumn/addcolumn.py
@@ -0,0 +1,18 @@
+import pymysql
+
+conn = pymysql.connect(host='localhost', user='root', password='', db='imdb', charset='utf8', cursorclass=pymysql.cursors.DictCursor)
+
+conn = pymysql.connect(host='localhost', user='root', password='', db='imdb', charset='utf8', cursorclass=pymysql.cursors.DictCursor)
+
+if __name__ == "__main__":
+    with conn.cursor() as cursor:
+      # sql = "ALTER TABLE actors ADD num_of_films INT"
+      # cursor.execute(sql)
+      # cursor.commit()
+      sql = "SELECT actor_id, count(movie_id) FROM cast GROUP BY actor_id"
+      cursor.execute(sql)
+      all_films_by_id = cursor.fetchall()
+      for i in range(len(all_films_by_id)):
+        sql = "UPDATE actors SET num_of_films = {} WHERE id='{}'".format(all_films_by_id[i]["count(movie_id)"],all_films_by_id[i]["actor_id"])
+        cursor.execute(sql)
+      conn.commit()
