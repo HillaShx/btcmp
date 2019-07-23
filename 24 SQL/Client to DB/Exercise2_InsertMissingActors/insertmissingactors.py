4507d0fdc92c7b01d86cdd5bb970d3f6eb6f9656
HillaShx <hilla.sh@gmail.com>
2019-07-23 15:23:28 +0300

    added validation for duplicate IDs

--- 24 SQL/Client to DB/Exercise2_InsertMissingActors/insertmissingactors.py
+++ 24 SQL/Client to DB/Exercise2_InsertMissingActors/insertmissingactors.py
@@ -9,14 +9,18 @@ acts = exi_acts + new_acts
 if __name__ == "__main__":
     try:
       with conn.cursor() as cursor:
+        cursor.execute("SELECT id from actors")
+        all_ids = [act_id['id'] for act_id in cursor.fetchall()]
         for name in acts:
           sql = 'SELECT * from actors WHERE (id={} AND full_name="{}" AND gender="{}")'.format(name["id"],name["full_name"],name["gender"])
           cursor.execute(sql)
           result = cursor.fetchone()
           if not result:
-            sql = 'INSERT INTO actors VALUES ("{}","{}","{}");'.format(name["id"],name["full_name"],name["gender"])
-            print(sql)
-            cursor.execute(sql)
+            if name["id"] in all_ids:
+              print("Duplicate ID. This is not allowed!")
+            else:
+              sql = 'INSERT INTO actors VALUES ("{}","{}","{}");'.format(name["id"],name["full_name"],name["gender"])
+              cursor.execute(sql)
         conn.commit()
     except:
       print("failed")
