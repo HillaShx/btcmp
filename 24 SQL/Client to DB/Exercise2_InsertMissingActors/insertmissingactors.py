b7d1ad0e2206347c1997ec37171db16d50b61433
HillaShx <hilla.sh@gmail.com>
2019-07-23 15:05:59 +0300

    exercise2_insertmissintactors

---
+++ 24 SQL/Client to DB/Exercise2_InsertMissingActors/insertmissingactors.py
@@ -0,0 +1,24 @@
+import pymysql
+
+conn = pymysql.connect(host='localhost', user='root', password='', db='imdb', charset='utf8', cursorclass=pymysql.cursors.DictCursor)
+
+exi_acts = [{'id':'2547', 'full_name':'Andrew Adamson', 'gender':'M'},{'id':'533380', 'full_name':'Karen Allen', 'gender':'F'}]
+new_acts = [{'id':909009090, 'full_name':'Someone Someoneson', 'gender':'M'},{'id':9080708, 'full_name':'Someoneson Someone', 'gender':'F'}, {'id':901020300, 'full_name': 'Someone Sonsomeone', 'gender':'F'}]
+acts = exi_acts + new_acts
+
+if __name__ == "__main__":
+    try:
+      with conn.cursor() as cursor:
+        for name in acts:
+          sql = 'SELECT * from actors WHERE (id={} AND full_name="{}" AND gender="{}")'.format(name["id"],name["full_name"],name["gender"])
+          cursor.execute(sql)
+          result = cursor.fetchone()
+          if not result:
+            sql = 'INSERT INTO actors VALUES ("{}","{}","{}");'.format(name["id"],name["full_name"],name["gender"])
+            print(sql)
+            cursor.execute(sql)
+        conn.commit()
+    except:
+      print("failed")
+
+
