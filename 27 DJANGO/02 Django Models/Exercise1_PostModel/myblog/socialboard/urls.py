d08989b36cd31f343e4e8adda0d69008a06bd5ae
HillaShx <hilla.sh@gmail.com>
2019-08-04 11:47:04 +0300

    not sure what changed

---
+++ 27 DJANGO/02 Django Models/Exercise1_PostModel/myblog/socialboard/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'socialboard'
+
+urlpatterns = [
+  url(r'^$', views.index, name="index"),
+]
---
+++ 27 DJANGO/02 Django Models/Exercise1_PostModel/myblog/socialboard/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+
+def index(request):
+  posts = Post.objects.all()
+  context = {"posts": posts}
+  return render(request, "socialboard/index.html", context)
