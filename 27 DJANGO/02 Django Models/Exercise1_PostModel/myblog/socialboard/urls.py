f77189e0b283c3b4b83a4a9bfd09a4ddcc0c3730
HillaShx <hilla.sh@gmail.com>
2019-08-04 14:53:20 +0300

    exercise1_userauthindjango

--- 27 DJANGO/02 Django Models/Exercise1_PostModel/myblog/socialboard/urls.py
+++ 27 DJANGO/02 Django Models/Exercise1_PostModel/myblog/socialboard/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth.views import login
 
 app_name = 'socialboard'
 
 urlpatterns = [
   url(r'^$', views.index, name="index"),
+  url(r'^register/$', views.register, name="register"),
+  url(r'^login/$', login, name="login"),
 ]
--- 27 DJANGO/02 Django Models/Exercise1_PostModel/myblog/socialboard/views.py
+++ 27 DJANGO/02 Django Models/Exercise1_PostModel/myblog/socialboard/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm
 from .models import Post
 
 # Create your views here.
@@ -8,3 +10,29 @@ def index(request):
   posts = Post.objects.all()
   context = {"posts": posts}
   return render(request, "socialboard/index.html", context)
+
+def register(request):
+  if request.method == "POST":
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      new_user = form.save()
+    return HttpResponse("not valid")
+  else:
+    form = UserCreationForm()
+  return render(request, "registration/register.html", {"form": form})
+
+def login(request):
+  if request.method == "POST":
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+      if user.is_active:
+        login(request, user)
+        # redirect
+      else:
+        return HttpResponse("user is not active")
+    else:
+      return HttpResponse("Invalid credentials")
+  else:
+    return render(request, "registration/login.html", {"next": "/"})
