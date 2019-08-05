df855a9e20194e4deeef40704e770fb95d6bdd1d
HillaShx <hilla.sh@gmail.com>
2019-08-05 16:26:05 +0300

    took care of basic+ layout and design

---
+++ 00 Assignment13_Billboard/myblog/socialboard/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+from django.contrib.auth.views import login, logout
+
+app_name = 'socialboard'
+
+urlpatterns = [
+  url(r'^$', views.index, name="index"),
+  url(r'^register/$', views.register, name="register"),
+  url(r'^login/$', login, name="login"),
+  url(r'^logout/$', logout, name="logout"),
+]
---
+++ 00 Assignment13_Billboard/myblog/socialboard/views.py
@@ -0,0 +1,42 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm
+from .models import Post
+
+# Create your views here.
+
+def index(request):
+  if not request.user.is_anonymous:
+    user = authenticate(username=request.user.username, password=request.user.password)
+    posts = Post.objects.all()
+    context = {"posts": posts}
+    return render(request, "socialboard/index.html", context)
+  else:
+    return redirect(reverse('socialboard:login') + "?next=/")
+
+def register(request):
+  if request.method == "POST":
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      new_user = form.save()
+    return HttpResponse("not valid")
+  else:
+    form = UserCreationForm()
+  return render(request, "registration/register.html", {"form": form})
+
+def login(request):
+  if request.method == "POST":
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+      if user.is_active:
+        login(request, user)
+        # redirect
+      else:
+        return HttpResponse("user is not active")
+    else:
+      return HttpResponse("Invalid credentials")
+  else:
+    return render(request, "registration/login.html", {"next": request.GET.get("next", "/")})
